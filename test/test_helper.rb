ac534b777b630d70f1d94a218624c2521c19ac6a
bichht-0889 <hoang.thi.bich@sun-asterisk.com>
2019-05-02 08:38:51 +0700

    Chapter 8..

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,5 +5,8 @@ require "rails/test_help"
 class ActiveSupport::TestCase
   fixtures :all
   include ApplicationHelper
-  # Add more helper methods to be used by all tests here...
+
+  def is_logged_in?
+    !session[:user_id].present?
+  end
 end
